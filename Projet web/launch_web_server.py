3bdabce860f990ec694464f140d2cbcd2a418e03
Minicapt <boox@hotmail.fr>
2020-09-23 12:06:46 +0200

    doc web
    Doc web

---
+++ Projet web/launch_web_server.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 22 18:46:04 2020
+
+@author: amaury
+"""
+import http.server
+import socketserver
+
+PORT = 8080
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
