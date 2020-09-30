31ab3c7de6c2456dc9792163cf570e82dce2a3bb
Minicapt <boox@hotmail.fr>
2020-09-30 19:15:25 +0200

    travail sur le style des lignes de métro

--- Projet web/humanresources/humanresources/js/map_scripts.js
+++ Projet web/humanresources/humanresources/js/map_scripts.js
@@ -14,7 +14,7 @@ var map = new L.Map("map", {
 });
 
 // Ajout de Features à la carte
-var station =
+var station1 =
   {
     "type":"Feature",
     "properties":{
@@ -35,13 +35,39 @@ var station =
       }
   };
 
-L.geoJSON(station).addTo(map);
+  var station2 =
+    {
+      "type":"Feature",
+      "properties":{
+        "ID":"1842",
+        "STATION":"GARE DU NORD",
+        "CITY":"Paris",
+        "QUARTER":"10",
+        "TRAFIC":"49977513",
+        "LINES":"4-5",
+        "COLORS":"#BB4D98-#DE8B53"
+        },
+        "geometry":{
+          "type":"Point",
+          "coordinates":[
+             2.36470307836603,
+             48.8799654432891
+          ]
+        }
+    };
+
+// L.geoJSON(station).addTo(map);
 
 $.getJSON("../../Data Paris/Stations/ligne_metro.geojson", function(data) {
   var dataLayer = L.geoJson(data, {
         onEachFeature: function(feature, layer) {
             var popupText = "Ligne: " + feature.properties.route_short_name;
             layer.bindPopup(popupText);
+            layer.on({
+                mouseover: highlightFeature,
+                // mouseout: resetHighlight,
+                click: zoomToFeature
+            });
           },
         style: function(feature){
           var color = "#" + feature.properties.route_color;
@@ -52,3 +78,35 @@ $.getJSON("../../Data Paris/Stations/ligne_metro.geojson", function(data) {
         });
   dataLayer.addTo(map);
   });
+
+
+  function zoomToFeature(e) {
+      map.fitBounds(e.target.getBounds());
+  };
+
+  function highlightFeature(e) {
+      var layer = e.target;
+
+      layer.setStyle({
+          weight: 5,
+          color: '#666',
+          dashArray: '',
+          fillOpacity: 0.7
+      });
+
+      if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+          layer.bringToFront();
+      }
+  };
+
+  function resetHighlight(e) {
+}
+
+  var years = [station1, station2];
+  layerGroup = L.layerGroup(years);
+  var sliderControl = L.Control.SliderControl({
+    layer: layerGroup,
+    follow: true
+  });
+  map.addControl(sliderControl);
+  sliderControl.startSlider();
