a7a967a8cbc3cd33183eb14f80471a07ceecbe31
Minicapt <49745555+Minicapt@users.noreply.github.com>
2020-10-21 15:43:58 +0200

    ajout 2 charts

--- Visualisation 3/tidy-tree/5432439324f2c616@170.js
+++ Visualisation 3/tidy-tree/5432439324f2c616@170.js
@@ -1,7 +1,7 @@
 // https://observablehq.com/@d3/tidy-tree@170
 export default function define(runtime, observer) {
   const main = runtime.module();
-  const fileAttachments = new Map([["flare-2.json",new URL("./files/e65374209781891f37dea1e7a6e1c5e020a3009b8aedf113b4c80942018887a1176ad4945cf14444603ff91d3da371b3b0d72419fa8d2ee0f6e815732475d5de",import.meta.url)]]);
+  const fileAttachments = new Map([["flare-2.json",new URL("../flare-2.json",import.meta.url)]]);
   main.builtin("FileAttachment", runtime.fileAttachments(name => fileAttachments.get(name)));
   main.variable(observer()).define(["md"], function(md){return(
 md`# Tidy Tree
@@ -21,12 +21,12 @@ D3’s [tree layout](https://github.com/d3/d3-hierarchy/blob/master/README.md#tr
 
   const svg = d3.create("svg")
       .attr("viewBox", [0, 0, width, x1 - x0 + root.dx * 2]);
-  
+
   const g = svg.append("g")
       .attr("font-family", "sans-serif")
       .attr("font-size", 10)
       .attr("transform", `translate(${root.dy / 3},${root.dx - x0})`);
-    
+
   const link = g.append("g")
     .attr("fill", "none")
     .attr("stroke", "#555")
@@ -38,7 +38,7 @@ D3’s [tree layout](https://github.com/d3/d3-hierarchy/blob/master/README.md#tr
       .attr("d", d3.linkHorizontal()
           .x(d => d.y)
           .y(d => d.x));
-  
+
   const node = g.append("g")
       .attr("stroke-linejoin", "round")
       .attr("stroke-width", 3)
@@ -58,7 +58,7 @@ D3’s [tree layout](https://github.com/d3/d3-hierarchy/blob/master/README.md#tr
       .text(d => d.data.name)
     .clone(true).lower()
       .attr("stroke", "white");
-  
+
   return svg.node();
 }
 );
